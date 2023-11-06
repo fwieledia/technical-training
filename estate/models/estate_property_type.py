1623eeb5dceb8ff5834dc3c645f8efd71fc0fca4
fw <fw@eledia.de>
2023-11-06 15:55:59 +0100

    .

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -2,5 +2,8 @@ from odoo import models, fields
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
+    _description = "Property Type"
 
     name = fields.Char(required=True)
+
+    property_ids = fields.One2many("estate.property")
