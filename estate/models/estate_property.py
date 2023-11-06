01001b9d055be6f5d0476e3b250e3f3540565338
fw <fw@eledia.de>
2023-11-06 13:09:56 +0100

    .

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+# from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: self._default_date())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +21,6 @@ class EstateProperty(models.Model):
         string="garden orientation",
         selection=[("n", "North"), ("s", "South"), ("e", "East"), ("w", "West")]
     )
+
+    def _default_date(self):
+        return fields.Date.add(fields.Date.context_today(self), months=3)
