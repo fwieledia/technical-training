8c42d9c26ab79dac341a52f0e0056eb05738feae
fw <fw@eledia.de>
2023-11-06 16:20:02 +0100

    .

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,7 @@
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
-        "Views/estate_property_tag_views.xml"
+        "Views/estate_property_tag_views.xml",
         "views/estate_menus.xml",
     ],
     "installable": True,
