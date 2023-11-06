44acbed59008dd16746e144b01b3965efedae95d
fw <fw@eledia.de>
2023-11-06 16:37:08 +0100

    .

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -5,4 +5,13 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "estate offer"
 
-    name = fields.Char(required=True)
+    price = fields.Float()
+    status = fields.Selection(
+        selection=[
+            ("a", "Accepted"),
+            ("r", "Refused")
+        ],
+        copy=False
+    )
+    partner_id = fields.Many2one("res.partner", required=True)
+    property_id = fields.Many2one("estate.property", required=True)
--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -5,13 +5,4 @@ class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "estate tag"
 
-    price = fields.Float()
-    status = fields.Selection(
-        selection=[
-            ("a", "Accepted"),
-            ("r", "Refused")
-        ],
-        copy=False
-    )
-    partner_id = fields.Many2one("res.partner", required=True)
-    property_id = fields.Many2one("estate.property", required=True)
+    name = fields.Char(required=True)
