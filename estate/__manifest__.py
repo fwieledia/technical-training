06f3f1f477cc5737ab467d4202c6415ccf051f00
fw <fw@eledia.de>
2023-11-06 15:40:23 +0100

    .

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,10 +4,10 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-        "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_menus.xml",
+        "security/ir.model.access.csv",
     ],
     "installable": True,
     "license": "LGPL-3",
