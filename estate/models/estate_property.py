04e6a6bc2cdb3dfb6edc5330edd3189692b3a25c
fw <fw@eledia.de>
2023-11-06 16:09:00 +0100

    .

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -38,7 +38,7 @@ class EstateProperty(models.Model):
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     user_id = fields.Many2one("res.partner", string="Salesman", default=lambda self: self.env.user)
-    buyer_id = fields.Many2one("res.user", string="Buyer", copy=False)
+    buyer_id = fields.Many2one("res.users", string="Buyer", copy=False)
 
     def _default_date(self):
         return fields.Date.add(fields.Date.context_today(self), months=3)
